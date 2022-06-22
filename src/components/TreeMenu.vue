ab4850e0d4247715699edca73e38f116d5780884
zsh <1659583514@qq.com>
2022-06-22 17:23:31 +0800

    treemenu add div tag

--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -1,25 +1,29 @@
-<template v-for="menu in userMenu">
-  <el-submenu
-    v-if="
-      menu.children &&
-      menu.children.length > 0 &&
-      menu.children[0].menuType == 1
-    "
-    :key="menu._id"
-    :index="menu.path"
-  >
-    <template #title>
-      <i :class="menu.icon"></i>
-      <span>{{ menu.menuName }}</span>
+<template>
+  <div>
+    <template v-for="menu in userMenu">
+      <el-submenu
+        v-if="
+          menu.children &&
+          menu.children.length > 0 &&
+          menu.children[0].menuType == 1
+        "
+        :key="menu._id"
+        :index="menu.path"
+      >
+        <template #title>
+          <i :class="menu.icon"></i>
+          <span>{{ menu.menuName }}</span>
+        </template>
+        <tree-menu :userMenu="menu.children" />
+      </el-submenu>
+      <el-menu-item
+        v-else-if="menu.menuType == 1"
+        :index="menu.path"
+        :key="menu._id"
+        >{{ menu.menuName }}</el-menu-item
+      >
     </template>
-    <tree-menu :userMenu="menu.children" />
-  </el-submenu>
-  <el-menu-item
-    v-else-if="menu.menuType == 1"
-    :index="menu.path"
-    :key="menu._id"
-    >{{ menu.menuName }}</el-menu-item
-  >
+  </div>
 </template>
 <script>
 export default {
